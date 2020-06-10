2ec87819d94b9199cb0d7963249bf9fb7d7ae012
mark <913804005@qq.com>
2020-06-10 14:10:58 +0800

    增加 csrf 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
 
 class UsersController extends Controller
 {
@@ -17,8 +17,13 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-    public function index(User $user)
+    public function store(Request $request)
     {
-        return view('users.show', compact('user'));
+        $this->validate($request, [
+            'name' => 'required|unique:users|max:50',
+            'email' => 'required|email|unique:users|max:255',
+            'password' => 'required|confirmed|min:6'
+        ]);
+        return;
     }
 }
