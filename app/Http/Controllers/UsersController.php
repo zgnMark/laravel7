c528a707103edbed4caeab1b56872890bd4a6941
mark <913804005@qq.com>
2020-06-10 15:10:29 +0800

    登陆成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,7 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\Models\User;
+use Auth;
 
 class UsersController extends Controller
 {
@@ -20,7 +21,7 @@ class UsersController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'name' => 'required|unique:users|max:50',
+            'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
@@ -30,6 +31,8 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect()->route('users.show', [$user]);
     }
--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -26,3 +26,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
+try {
+    window.$ =window.jQuery =require('jquery');
+    require('bootstrap');
+} catch(e){}
