f2521a9abf162f6e62d42e71f79e78425f955984
mark <913804005@qq.com>
2020-06-10 12:00:21 +0800

    用户注册表单

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function create()
     {
         return view('users.create');
+    }
+
+    public function show(User $user)
+    {
+        return view('users.show', compact('user'));
+    }
+
+    public function index(User $user)
+    {
+        return view('users.show', compact('user'));
     }
 }
